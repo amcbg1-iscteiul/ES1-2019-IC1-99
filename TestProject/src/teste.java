2d479692b9d61a0914323a9fdbdbeb17bc778c85
amcbg1-iscteiul <antonio.b.gaspar@hotmail.com>
2019-10-11 18:55:29 +0100

    outro commit

--- TestProject/src/teste.java
+++ TestProject/src/teste.java
@@ -2,7 +2,7 @@
 public class teste {
 
 	public static void main(String[] args) {
-		System.out.println("Hello worldS");
+		System.out.println("Hello WOOOOOOOOOOOOOORLD");
 		// TODO Auto-generated method stub
 
 	}
