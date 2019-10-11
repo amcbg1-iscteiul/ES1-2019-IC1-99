2951e23e5873ba1ad9761f7dea4621c3a164c4fb
amcbg1-iscteiul <antonio.b.gaspar@hotmail.com>
2019-10-11 18:57:32 +0100

    olaaaa

--- TestProject/src/teste.java
+++ TestProject/src/teste.java
@@ -2,7 +2,7 @@
 public class teste {
 
 	public static void main(String[] args) {
-		System.out.println("Hello WOOOOOOOOOOOOOORLD");
+		System.out.println("Hecdllo WOOOOOOOOOOOOOORLD");
 		// TODO Auto-generated method stub
 
 	}
